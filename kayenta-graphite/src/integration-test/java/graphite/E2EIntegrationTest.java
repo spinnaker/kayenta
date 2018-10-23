7c91b64325c5b26dc62e00ed98bf751bc5f53a0a
Yuanchen Bai <ybai2@snapchat.com>
2018-10-22 17:14:39 -0700

    Revert "Revert "wip add integration test framework""
    This reverts commit 8465c4a42abf68b939e997be802683923ca260dd.

---
+++ kayenta-graphite/src/integration-test/java/graphite/E2EIntegrationTest.java
@@ -0,0 +1,12 @@
+package graphite;
+
+import io.restassured.response.ValidatableResponse;
+
+import java.io.IOException;
+
+public class E2EIntegrationTest {
+
+    private ValidatableResponse doCanaryExec(String scope) throws IOException {
+
+    }
+}
---
+++ kayenta-graphite/src/integration-test/java/graphite/GraphiteIntegrationTestConfig.java
@@ -0,0 +1,9 @@
+package graphite;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration
+@Slf4j
+public class GraphiteIntegrationTestConfig {
+}
