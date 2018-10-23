8465c4a42abf68b939e997be802683923ca260dd
Yuanchen Bai <ybai2@snapchat.com>
2018-10-22 17:14:13 -0700

    Revert "wip add integration test framework"
    This reverts commit ddce4f5adc39e48b0f11a60cd605645d8b2273f0.

--- kayenta-graphite/src/integration-test/java/graphite/GraphiteIntegrationTestConfig.java
+++
@@ -1,9 +0,0 @@
-package graphite;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration
-@Slf4j
-public class GraphiteIntegrationTestConfig {
-}
