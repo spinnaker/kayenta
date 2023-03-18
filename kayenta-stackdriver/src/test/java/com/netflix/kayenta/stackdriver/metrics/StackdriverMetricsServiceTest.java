34ed6b2c8995073cc325fe606fa3809c3fd624b9
Tom Klinovsky <tom.klino@gmail.com>
2023-03-18 15:34:24 +0200

    WIP: adding tests to StackdriverMetricsService

--- kayenta-stackdriver/src/test/java/com/netflix/kayenta/stackdriver/metrics/StackdriverMetricsServiceTest.java
+++ kayenta-stackdriver/src/test/java/com/netflix/kayenta/stackdriver/metrics/StackdriverMetricsServiceTest.java
@@ -1,14 +1,20 @@
 package com.netflix.kayenta.stackdriver.metrics;
 
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
 
-import com.netflix.kayenta.canary.CanaryMetricConfig;
-import com.netflix.kayenta.canary.CanaryScope;
-import com.netflix.kayenta.canary.providers.metrics.StackdriverCanaryMetricSetQueryConfig;
-import com.netflix.kayenta.stackdriver.canary.StackdriverCanaryScope;
+import com.google.api.services.monitoring.v3.model.ListTimeSeriesResponse;
+import com.google.api.services.monitoring.v3.model.Point;
 import com.google.api.services.monitoring.v3.model.TimeSeries;
-
+import com.google.api.services.monitoring.v3.model.TypedValue;
+import com.netflix.kayenta.canary.CanaryConfig;
+import com.netflix.kayenta.canary.CanaryMetricConfig;
+import com.netflix.kayenta.metrics.MetricSet;
+import com.netflix.kayenta.stackdriver.canary.StackdriverCanaryScope;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -17,13 +23,13 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class StackdriverMetricsServiceTest {
-  private static final String METRIC_NAME = "example.metric.name";
+  private static final String ACCOUNT_NAME = "some-stackdriver-account";
   private static final String SCOPE = "env=test";
-  private static final String AGGREGATE = "avg";
+  private static final double testDoubleValue = 12.3d;
 
   @Mock ListTimeSeriesResponse response;
 
-  @InjectMocs StackdriverMetricsService stackdriverMetricsService;
+  @InjectMocks StackdriverMetricsService stackdriverMetricsService;
 
   @Test
   public void testDummy() {
@@ -32,14 +38,32 @@ public class StackdriverMetricsServiceTest {
 
   @Test
   public void returnsMetricSetValuesFromDouble() {
+    TimeSeries singleDoublePointTimeSeries =
+        new TimeSeries()
+            .setValueType("DOUBLE")
+            .setPoints(
+                new ArrayList<Point>() {
+                  {
+                    add((new Point()).setValue((new TypedValue()).setDoubleValue(testDoubleValue)));
+                  }
+                });
+
     when(response.getTimeSeries())
-      .thenReturn(
-        // TODO construct List<TimeSeries> that returns points with double values
-      );
+        .thenReturn(
+            new ArrayList<TimeSeries>() {
+              {
+                add(singleDoublePointTimeSeries);
+              }
+            });
 
-    List<MetricSet> metricSetList = stackdriverMetricsService.queryMetrics(/*TODO which args?*/);
+    CanaryConfig canaryConfig = new CanaryConfig();
+    CanaryMetricConfig canaryMetricConfig = CanaryMetricConfig.builder().name("some-name").build();
+    StackdriverCanaryScope experimentScope = new StackdriverCanaryScope();
 
-    assertThat(metricSetList, is(/*TODO expected metricSetList*/)
+    List<MetricSet> metricSetList =
+        stackdriverMetricsService.queryMetrics(
+            ACCOUNT_NAME, canaryConfig, canaryMetricConfig, experimentScope);
+
+    assertThat(metricSetList.get(0).getValues(), hasItem(testDoubleValue));
   }
 }
-
