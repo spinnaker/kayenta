d29fff98aeacdfa3846e335a84f1e247f921dcf5
Tom Klinovsky <tomklino@google.com>
2023-12-26 00:10:38 +0200

    added tests for stackdriverMetricService

--- kayenta-stackdriver/src/test/java/com/netflix/kayenta/stackdriver/metrics/StackdriverMetricsServiceTest.java
+++ kayenta-stackdriver/src/test/java/com/netflix/kayenta/stackdriver/metrics/StackdriverMetricsServiceTest.java
@@ -1,69 +1,153 @@
 package com.netflix.kayenta.stackdriver.metrics;
 
-import static org.hamcrest.CoreMatchers.hasItem;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Answers.RETURNS_DEEP_STUBS;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
+import com.google.api.services.monitoring.v3.Monitoring;
+import com.google.api.services.monitoring.v3.model.ListMetricDescriptorsResponse;
 import com.google.api.services.monitoring.v3.model.ListTimeSeriesResponse;
+import com.google.api.services.monitoring.v3.model.MetricDescriptor;
 import com.google.api.services.monitoring.v3.model.Point;
+import com.google.api.services.monitoring.v3.model.TimeInterval;
 import com.google.api.services.monitoring.v3.model.TimeSeries;
 import com.google.api.services.monitoring.v3.model.TypedValue;
 import com.netflix.kayenta.canary.CanaryConfig;
 import com.netflix.kayenta.canary.CanaryMetricConfig;
+import com.netflix.kayenta.canary.CanaryScope;
+import com.netflix.kayenta.canary.providers.metrics.StackdriverCanaryMetricSetQueryConfig;
+import com.netflix.kayenta.google.security.GoogleNamedAccountCredentials;
 import com.netflix.kayenta.metrics.MetricSet;
+import com.netflix.kayenta.security.AccountCredentials;
+import com.netflix.kayenta.security.AccountCredentialsRepository;
 import com.netflix.kayenta.stackdriver.canary.StackdriverCanaryScope;
+import com.netflix.spectator.api.Registry;
+import java.io.IOException;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import java.util.Map;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class StackdriverMetricsServiceTest {
-  private static final String ACCOUNT_NAME = "some-stackdriver-account";
-  private static final String SCOPE = "env=test";
-  private static final double testDoubleValue = 12.3d;
 
-  @Mock ListTimeSeriesResponse response;
+  private static final String ACCOUNT = "test-account";
+
+  @Mock AccountCredentialsRepository accountCredentialsRepoMock;
+
+  @Mock(answer = RETURNS_DEEP_STUBS)
+  GoogleNamedAccountCredentials googleAccountCredentialsMock;
+
+  @Mock GoogleNamedAccountCredentials stackdriverCredentialsMock;
+  @Mock ListMetricDescriptorsResponse listMetricDescriptorsResponseMock;
+  @Mock StackdriverCanaryScope canaryScopeMock;
+
+  @Mock(answer = RETURNS_DEEP_STUBS)
+  Monitoring monitoringMock;
+
+  @Mock Monitoring.Projects.TimeSeries.List timeSeriesListMock;
+  @Mock ListTimeSeriesResponse responseMock;
+
+  @Mock(answer = RETURNS_DEEP_STUBS)
+  Registry registry;
 
   @InjectMocks StackdriverMetricsService stackdriverMetricsService;
 
   @Test
-  public void testDummy() {
-    assertThat("dummy", is("dummy"));
+  public void readsInt64Metrics() throws IOException {
+    when(accountCredentialsRepoMock.getRequiredOne(ACCOUNT)).thenReturn(stackdriverCredentialsMock);
+
+    when(stackdriverCredentialsMock.getMonitoring()).thenReturn(monitoringMock);
+
+    when(monitoringMock
+            .projects()
+            .timeSeries()
+            .list(anyString())
+            .setAggregationAlignmentPeriod(anyString())
+            .setAggregationCrossSeriesReducer(anyString())
+            .setAggregationPerSeriesAligner(anyString())
+            .setFilter(anyString())
+            .setIntervalStartTime(anyString())
+            .setIntervalEndTime(anyString()))
+        .thenReturn(timeSeriesListMock);
+
+    when(timeSeriesListMock.execute()).thenReturn(responseMock);
+
+    List<TimeSeries> timeSeriesListWithInt64Points = new ArrayList<TimeSeries>();
+
+    // Create a time series with INT64 points
+    List<Point> int64Points = new ArrayList<Point>();
+    int64Points.add(
+        new Point()
+            .setValue(new TypedValue().setInt64Value((Long) 64l))
+            .setInterval(
+                new TimeInterval()
+                    .setStartTime("1970-01-01T00:00:00.00Z")
+                    .setEndTime("1970-01-01T00:00:01.00Z")));
+    TimeSeries timeSeriesWithInt64Points =
+        new TimeSeries().setValueType("INT64").setPoints(int64Points);
+    timeSeriesListWithInt64Points.add(timeSeriesWithInt64Points);
+
+    when(responseMock.getTimeSeries()).thenReturn(timeSeriesListWithInt64Points);
+
+    CanaryConfig canaryConfig = new CanaryConfig();
+    CanaryMetricConfig canaryMetricConfig =
+        CanaryMetricConfig.builder()
+            .name("metricConfig")
+            .query(
+                StackdriverCanaryMetricSetQueryConfig.builder()
+                    .resourceType("global")
+                    .metricType("instance")
+                    .build())
+            .build();
+
+    CanaryScope canaryScope =
+        new StackdriverCanaryScope()
+            .setStart(Instant.EPOCH)
+            .setEnd(Instant.EPOCH.plusSeconds(1))
+            .setStep(1l);
+    List<MetricSet> queriedMetrics =
+        stackdriverMetricsService.queryMetrics(
+            ACCOUNT, canaryConfig, canaryMetricConfig, canaryScope);
+
+    assertThat(queriedMetrics.get(0).getValues()).contains(64d);
   }
 
   @Test
-  public void returnsMetricSetValuesFromDouble() {
-    TimeSeries singleDoublePointTimeSeries =
-        new TimeSeries()
-            .setValueType("DOUBLE")
-            .setPoints(
-                new ArrayList<Point>() {
-                  {
-                    add((new Point()).setValue((new TypedValue()).setDoubleValue(testDoubleValue)));
-                  }
-                });
+  public void returnsSingleMetricDescriptorInCache() throws IOException {
+    Set<AccountCredentials> accountCredentialsSetMock = new HashSet<>();
+    accountCredentialsSetMock.add(googleAccountCredentialsMock);
 
-    when(response.getTimeSeries())
-        .thenReturn(
-            new ArrayList<TimeSeries>() {
-              {
-                add(singleDoublePointTimeSeries);
-              }
-            });
+    when(accountCredentialsRepoMock.getAllOf(AccountCredentials.Type.METRICS_STORE))
+        .thenReturn(accountCredentialsSetMock);
 
-    CanaryConfig canaryConfig = new CanaryConfig();
-    CanaryMetricConfig canaryMetricConfig = CanaryMetricConfig.builder().name("some-name").build();
-    StackdriverCanaryScope experimentScope = new StackdriverCanaryScope();
+    when(googleAccountCredentialsMock
+            .getMonitoring()
+            .projects()
+            .metricDescriptors()
+            .list(anyString())
+            .execute())
+        .thenReturn(listMetricDescriptorsResponseMock);
 
-    List<MetricSet> metricSetList =
-        stackdriverMetricsService.queryMetrics(
-            ACCOUNT_NAME, canaryConfig, canaryMetricConfig, experimentScope);
+    List<MetricDescriptor> metricDesciprtorMockList = new ArrayList<MetricDescriptor>();
 
-    assertThat(metricSetList.get(0).getValues(), hasItem(testDoubleValue));
+    MetricDescriptor exampleMetricDescriptor = new MetricDescriptor();
+    metricDesciprtorMockList.add(exampleMetricDescriptor);
+    when(listMetricDescriptorsResponseMock.getMetricDescriptors())
+        .thenReturn(metricDesciprtorMockList);
+
+    stackdriverMetricsService.updateMetricDescriptorsCache();
+
+    List<Map> metadata = stackdriverMetricsService.getMetadata(ACCOUNT, "");
+
+    assertThat(metadata).containsOnly(exampleMetricDescriptor);
   }
 }
